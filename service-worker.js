419157b8ce51ae3d66a1056e29044f2c81bd7472
Gerson Sibinde <gersonsibinde3@gmail.com>
2025-09-10 01:22:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,6 @@
+// This is a minimal service worker to make the app installable.
+self.addEventListener('fetch', (event) => {
+  // This event listener is required for the PWA install prompt to appear.
+  // We are not adding any caching logic here, but it could be added in the future.
+});
  
